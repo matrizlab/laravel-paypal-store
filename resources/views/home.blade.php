87f9b7ce899a8d6639bb123e145640429cabbd5e
matrizlab <info@matrizlab.com>
2020-02-09 22:19:46 +0100

    Update Views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="container">
+        @if(Session::has('success'))
+            <div class="alert alert-success">
+                {{ Session::get('success') }}
+                @php
+                    Session::forget('success');
+                @endphp
             </div>
+        @endif
+
+        @if ($errors->any())
+            <div class="alert alert-warning" uk-alert>
+                {{ Session::get('error') }}
+                @php
+                    Session::forget('error');
+                @endphp
+            </div>
+        @endif
+        <div class="row">
+            @foreach($products as $product)
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <!-- The product name (duh..) -->
+                            {{ $product->name }}
+                        </div>
+                        <div class="card-body">
+                            <h3>
+                                <!-- We format the number to a price with currency behind it -->
+                                {{ number_format($product->price, 2) }} USD
+                            </h3>
+                            <a href="{{ route('add', [ $product->getRouteKey() ]) }}">
+                                <!-- The button for adding the product to the cart -->
+                                <button class="btn btn-primary">Add to cart</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
-</div>
 @endsection
