9eef81cdd9030048ac05e3caca22ed91d0e21e24
matrizlab <info@matrizlab.com>
2020-02-09 22:21:01 +0100

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,27 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# Default route when accessing the website
+Route::get('/', 'StoreController@index');
 
+# The home route, which is used in the authentication scaffolding
+# We update the closure argument to the index function of our controller
+Route::get('/home', 'StoreController@index')->name('home');
+
+# Adding a product to the shopping cart
+Route::get('/add/{product}', 'StoreController@addToCart')->name('add');
+
+# Removing an product from the shopping cart
+Route::get('/remove/{productId}', 'StoreController@removeProductFromCart')->name('remove');
+
+# Clearing all products from the shopping cart
+Route::get('/empty', 'StoreController@destroyCart')->name('empty');
+
+# PayPal checkout
+Route::get('checkout', 'PaypalController@payWithpaypal')->name('checkout');
+
+# PayPal status callback
+Route::get('status', 'PaypalController@getPaymentStatus');
+
+# Generated routes for authentication
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
